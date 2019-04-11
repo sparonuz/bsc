d7332a85f0ef47702eedcf1f962a1bb60e1ec96b
sparonuz <stella.paronuzzi@bsc.es>
2019-04-11 11:47:08 +0200

    the mean or std_dev of timesteps is now computed

--- scaling/compute_scale.sh
+++ scaling/compute_scale.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-out_file='pts.txt'
-cat /dev/null > $out_file
+f_proc='f_proc.txt'
+f_time_step='f_time.txt'
+cat /dev/null > $f_proc
+cat /dev/null > $f_time_step
+
 for NOP in `seq 48 48 576`
 do
   NEMO_PROC=$((NOP/48*46))
   folder=eOrca025_opt_$NEMO_PROC
   time_spent=($(cat $folder/steps.timing  | awk '{print $2}'))
-  echo  $NEMO_PROC " " `echo "${time_spent[-1]} - ${time_spent[0]}" | bc -l` >> $out_file
+  echo $NEMO_PROC >> $f_proc 
+  echo ${time_spent[@]} >> $f_time_step
+  #echo  $NEMO_PROC " " `echo "${time_spent[-1]} - ${time_spent[0]}" | bc -l` >> $out_file
 done
+module load python
+python plot.py  $f_proc $f_time_step
 
-python plot.py 
--- scaling/plot.py
+++ scaling/plot.py
@@ -1,19 +1,32 @@
 #!/usr/bin/python
-
+import sys
 import numpy as np
 from matplotlib import pyplot as plt
 
-pts = np.genfromtxt('pts.txt')
+f_proc=sys.argv[1]
+f_time_step=sys.argv[2]
+
+n_cores = np.genfromtxt(f_proc)
+time_step = np.genfromtxt(f_time_step)
 
 fig, ax = plt.subplots()
-n_cores=pts[:, 0]
-real_time=pts[:, 1]
+
+for i in range(0, time_step.shape[1]-1):
+  time_step[:, i] = time_step[:, i+1] - time_step[:, i]
+
+real_time =  np.mean(time_step[:,5:-5], axis=1)
 real_time = real_time/real_time[0]
+
+std_dev =   np.std(time_step[:,5:-5], axis=1)
+print(std_dev)
+std_dev = std_dev / std_dev[0]
+exit()
 ideal_curve =np.zeros(n_cores.shape[0])
 for i in range(1,n_cores.shape[0]+1) :
   ideal_curve[i-1] = n_cores[0]*1./n_cores[i-1]
+
 #ideal_curve=np.linspace(n_cores[0], n_cores[-1], n_cores.shape[0])
-ax.plot(n_cores, real_time, 'bs', n_cores, ideal_curve, 'r--')
+ax.plot(n_cores, real_time, 'bs', n_cores, ideal_curve, 'r--', n_cores, std_dev, 'b--')
 #ax.plot(n_cores, real_time, 'r--')
 
 ax.set(xlabel='N cores', ylabel='Normalized time(s)',  title='MN4 scaling curve NEMO4')
