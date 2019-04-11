31c5b60ba45a844d4087f7d7b7b7792777a8140a
sparonuz <stella.paronuzzi@bsc.es>
2019-04-11 15:14:06 +0200

    multiple run

--- scaling/compute_scale.sh
+++ scaling/compute_scale.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
-f_proc='f_proc.txt'
-f_time_step='f_time.txt'
-cat /dev/null > $f_proc
-cat /dev/null > $f_time_step
 
-for NOP in `seq 48 48 576`
+
+for repetition in 1 2 3 
 do
-  NEMO_PROC=$((NOP/48*46))
-  folder=eOrca025_opt_$NEMO_PROC
-  time_spent=($(cat $folder/steps.timing  | awk '{print $2}'))
-  echo $NEMO_PROC >> $f_proc 
-  echo ${time_spent[@]} >> $f_time_step
-  #echo  $NEMO_PROC " " `echo "${time_spent[-1]} - ${time_spent[0]}" | bc -l` >> $out_file
+  f_proc='f_proc'$repetition'.txt'
+  f_time_step='f_time'$repetition'.txt'
+  f_array[$repetition]=$f_time_step
+  f_proc_array[$repetition]=$f_proc
+  cat /dev/null > $f_proc
+  cat /dev/null > $f_time_step
+
+  for NOP in `seq 48 48 2304`
+  do
+
+    NEMO_PROC=$((NOP/48*46))
+    folder=eOrca025_opt_${repetition}_${NEMO_PROC}
+    if [[ -d $folder ]]
+    then 
+      time_spent=($(cat $folder/steps.timing  | awk '{print $2}'))
+      echo $NEMO_PROC >> $f_proc 
+      echo ${time_spent[@]} >> $f_time_step
+    fi
+  done
 done
+echo ${f_proc_array[@]} ${f_array[@]}
 module load python
-python plot.py  $f_proc $f_time_step
+python plot.py  ${f_proc_array[@]} ${f_array[@]}
 
--- scaling/plot.py
+++ scaling/plot.py
@@ -1,34 +1,46 @@
 #!/usr/bin/python
 import sys
+from random import randint
 import numpy as np
 from matplotlib import pyplot as plt
 
-f_proc=sys.argv[1]
-f_time_step=sys.argv[2]
 
-n_cores = np.genfromtxt(f_proc)
-time_step = np.genfromtxt(f_time_step)
+repetition= int(len(sys.argv)/2)
+f_proc=[]
+f_time_step = []
+print(repetition)
+colors = []
+colors=['b--','r--', 'gs','ys']
 
 fig, ax = plt.subplots()
 
-for i in range(0, time_step.shape[1]-1):
-  time_step[:, i] = time_step[:, i+1] - time_step[:, i]
+for i_rep in range(0 , repetition):
+  f_proc.append(sys.argv[i_rep+1])
+  f_time_step.append(sys.argv[repetition + i_rep + 1])
+  
+  n_cores = np.genfromtxt(f_proc[i_rep])
+  time_step = np.genfromtxt(f_time_step[i_rep])
+  for i in range(0, time_step.shape[1]-1):
+    time_step[:, i] = time_step[:, i+1] - time_step[:, i]
+  
+  real_time =  np.mean(time_step[:,5:-5], axis=1)
+  real_time = real_time/real_time[0]
+  
+  std_dev = np.std(time_step[:,5:-5], axis=1)
+  std_dev = std_dev / std_dev[0]
+  
+  ideal_curve =np.zeros(n_cores.shape[0])
+  print(f_proc, f_time_step,n_cores.shape,time_step.shape)
+  for i in range(1, n_cores.shape[0]+1):
+    ideal_curve[i-1] = n_cores[0]*1./n_cores[i-1]
 
-real_time =  np.mean(time_step[:,5:-5], axis=1)
-real_time = real_time/real_time[0]
-
-std_dev =   np.std(time_step[:,5:-5], axis=1)
-print(std_dev)
-std_dev = std_dev / std_dev[0]
-exit()
-ideal_curve =np.zeros(n_cores.shape[0])
-for i in range(1,n_cores.shape[0]+1) :
-  ideal_curve[i-1] = n_cores[0]*1./n_cores[i-1]
-
-#ideal_curve=np.linspace(n_cores[0], n_cores[-1], n_cores.shape[0])
-ax.plot(n_cores, real_time, 'bs', n_cores, ideal_curve, 'r--', n_cores, std_dev, 'b--')
-#ax.plot(n_cores, real_time, 'r--')
-
+#  colors.append('%06X' % randint(0, 0xFFFFFF))
+#  colors[i_rep] = '#'+ colors[i_rep] 
+  print(colors)
+  ax.plot(n_cores, real_time, colors[i_rep])
+  
+#ax.plot(n_cores, real_time, 'bs', n_cores, ideal_curve, 'r--', n_cores, std_dev, 'b--')
+ax.plot(n_cores, ideal_curve, 'g--')
 ax.set(xlabel='N cores', ylabel='Normalized time(s)',  title='MN4 scaling curve NEMO4')
 ax.grid()
 plt.show()
