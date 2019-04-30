53a8f5db3263d0e8d2f10ace92f55cddb08f5d34
sparonuz <stella.paronuzzi@bsc.es>
2019-04-30 10:32:35 +0200

    fix index for bash array.

--- scaling/compute_scale.sh
+++ scaling/compute_scale.sh
@@ -8,10 +8,9 @@ then
 fi
 
 experiment=$1
-
-for repetition in 1 2 3 
+rep_id=1
+for repetition in OCE_1 OCE_2 OCE_3 # ICE 
 do
-
   if [[ `ls | grep $experiment"_"$repetition | wc -l` -eq 0 ]] 
   then
     echo -e "No repetition n "${experiment}"_"$repetition" found.\nAborting" 
@@ -22,14 +21,15 @@ do
  
   f_proc='f_proc'$repetition'.txt'
   f_time_step='f_time'$repetition'.txt'
-  f_array[$repetition]=$f_time_step
-  f_proc_array[$repetition]=$f_proc
+  f_array[$rep_id]=$f_time_step
+  f_proc_array[$rep_id]=$f_proc
   cat /dev/null > $f_proc
   cat /dev/null > $f_time_step
-
+  rep_id=$((rep_id+1))
   for folder in ${i_experiment[@]} 
   do
-    echo $folder |  cut -d _ -f 3  >> $f_proc 
+    echo $folder |  awk -F_ '{print $NF}'   >> $f_proc 
+    #echo $folder |  cut -d _ -f 3  >> $f_proc 
     time_spent=($(cat $folder/steps.timing  | awk '{print $2}'))
     echo ${time_spent[@]} >> $f_time_step
   done
