7cfcd323386e398c41e8bf4f9e4b4a846158c46f
sparonuz <stella.paronuzzi@bsc.es>
2019-05-03 17:34:33 +0200

    BUGFIX:setting the proper var now disable outputs.

--- scaling/efficiency.sh
+++ scaling/efficiency.sh
@@ -6,10 +6,12 @@ RUN_FOLDER=EFFICIENCY_NEMO4_orig
 
 blue_print_job=nemo4_scaling.cmd
 
+#QUEUE="xlarge"
 QUEUE="bsc_es"
-TIME_STEP=200
 
-EXP_FOLDER=/gpfs/scratch/bsc32/bsc32402/NEMO4/run/RUN_FOLDER/Orca025_OCE_fullnode_\$NEMO_PROC
+TIME_STEP=1920
+
+EXP_FOLDER=/gpfs/scratch/bsc32/bsc32402/NEMO4/run/RUN_FOLDER/Orca025_OCE_5_NEMO_PROC
 #EXP_FOLDER=/gpfs/scratch/bsc32/bsc32402/NEMO4/run/RUN_FOLDER/Orca025_XIOS_2_\$NEMO_PROC
 
 #EXEC_FOLDER=/home/bsc32/bsc32402/local/Nemo/trunk-r10610/cfgs/ORCA2/EXP00/
@@ -21,6 +23,7 @@ EXEC_FOLDER=/home/bsc32/bsc32402/local/Nemo/trunk-r10610/cfgs/ORCA2_jpnij/EXP00/
 ICE=False
 #OUTPUT=True
 OUTPUT=False
+
 mkdir -p $RUN_FOLDER
 
 cp $blue_print_job  $RUN_FOLDER
@@ -41,35 +44,40 @@ XIOS=False
 
 if [[ $XIOS == True ]]
 then
-  XIOS_PROC=4
+  XIOS_PPN=4
 else
-  XIOS_PROC=0
+  XIOS_PPN=0
 fi
 
-PROC_PER_NODE=48
+#HIGHMEM=True
+HIGHMEM=False
 
-for nemo_NOP in   `seq 48 48 2016`
+PROC_PER_NODE=46
+ 
+for TOTAL_NP in  576  #`seq $((PROC_PER_NODE*51)) $((PROC_PER_NODE*4))  $((PROC_PER_NODE*100))`
 do
-  NOP=$((nemo_NOP+XIOS_PROC))
-#  echo $XIOS_PROC $nemo_NOP $NOP
-  job=job_$NOP.cmp
+  XIOS_PROC=$((XIOS_PPN*TOTAL_NP/PROC_PER_NODE))
+  NEMO_PROC=$((TOTAL_NP-XIOS_PROC))
+  # echo $XIOS_PROC $TOTAL_NP $NOP
+  job=job_$TOTAL_NP
   cp $blue_print_job $job 
+  if [[ $HIGHMEM == True ]]
+  then
+    sed -ri 's@HIGHMEM@SBATCH --constraint=highmem@' $job
+  fi
   sed -ri 's@QUEUE@'$QUEUE'@' $job
   sed -ri 's@TIME_STEP@'$TIME_STEP'@' $job
   sed -ri 's@EXP_FOLDER@'$EXP_FOLDER'@' $job
   sed -ri 's@EXEC_FOLDER@'$EXEC_FOLDER'@' $job
   sed -ri 's@RUN_FOLDER@'$RUN_FOLDER'@' $job
-  sed -ri 's@NOP@'$NOP'@' $job
-  sed -ri 's@\<XIOS\>@'$XIOS'@' $job
+  sed -ri 's@TOTAL_NP@'$TOTAL_NP'@' $job
+  sed -ri 's@NEMO_PROC@'$NEMO_PROC'@' $job
   sed -ri 's@XIOS_PROC@'$XIOS_PROC'@' $job
+  sed -ri 's@USE_XIOS@'$XIOS'@' $job
   sed -ri 's@PROC_PER_NODE@'$PROC_PER_NODE'@' $job
   sed -ri 's@ICE@'$ICE'@' $job
   sed -ri 's@OUTPUT@'$OUTPUT'@' $job
 
   sbatch $job 
-  if [[ $XIOS == True ]] 
-  then
-    XIOS_PROC=$((XIOS_PROC+4))
-  fi
 done
 cd .. 
--- scaling/nemo4_scaling.cmd
+++ scaling/nemo4_scaling.cmd
@@ -1,256 +1,83 @@
 #!/bin/bash
-###############################################################################
-#                             RUN NEMO
-###############################################################################
-#SBATCH --ntasks NOP 
-#SBATCH --ntasks-per-node PROC_PER_NODE 
-#SBATCH --job-name O025_NOP 
-#SBATCH --output efficiency_NOP_%j.o 
-#SBATCH --error  efficiency_NOP_%j.e
-##SBATCH -R "span[ptile=16]"
-#SBATCH --time 30:00
-#SBATCH --qos=QUEUE
-set -xv
 
-cd $SLURM_SUBMIT_DIR
+#RUN_FOLDER=EFFICIENCY_NEMO4_MPI+PAPI
+RUN_FOLDER=EFFICIENCY_NEMO4_orig
+#RUN_FOLDER=EFFICIENCY_NEMO4_func
 
-# Replace the number of resources used for NEMO and XIOS
-#ACTUAL_PROC=$((NOP/48*46))
-NEMO_PROC=$(((NOP-XIOS_PROC)/48*PROC_PER_NODE))
-time_step=TIME_STEP
+blue_print_job=nemo4_scaling.cmd
 
-# Export extrae variable (for the wrapper to know if it is activated)
-#XIOS=False
-xios=XIOS
+#QUEUE="xlarge"
+QUEUE="bsc_es"
 
-if [[ $xios == True ]]
-then
- xios_proc=XIOS_PROC 
-fi
+TIME_STEP=1920
 
-ice=ICE
+EXP_FOLDER=/gpfs/scratch/bsc32/bsc32402/NEMO4/run/RUN_FOLDER/Orca025_OCE_5_NEMO_PROC
+#EXP_FOLDER=/gpfs/scratch/bsc32/bsc32402/NEMO4/run/RUN_FOLDER/Orca025_XIOS_2_\$NEMO_PROC
 
+#EXEC_FOLDER=/home/bsc32/bsc32402/local/Nemo/trunk-r10610/cfgs/ORCA2/EXP00/
+EXEC_FOLDER=/home/bsc32/bsc32402/local/Nemo/trunk-r10610/cfgs/ORCA2_jpnij/EXP00/
+#EXEC_FOLDER=/home/bsc32/bsc32402/local/Nemo/trunk-r10610/cfgs/ORCA2-NETCDF-4.4.1.1/EXP00/
+#EXEC_FOLDER=/home/bsc32/bsc32402/local/Nemo/trunk-r10610/cfgs/ORCA2-xios-r1660/EXP00/
+#EXEC_FOLDER=/home/bsc32/bsc32402/local/Nemo/trunk-r10610/cfgs/ORCA025_ICE/EXP00/
 
+ICE=False
+#OUTPUT=True
+OUTPUT=False
 
-exec_name=nemo
-#exec_folder=/home/bsc32/bsc32402/local/Nemo/trunk-r10610/cfgs/ORCA2_scorep/EXP00/
-#exec_folder=/home/bsc32/bsc32402/local/Nemo/trunk-r10610/cfgs/ORCA2_fine_f/EXP00/
-#exec_folder=/home/bsc32/bsc32402/local/Nemo/trunk-r10610/cfgs/ORCA2_fine_f_3.5.4/EXP00/
-exec_folder=EXEC_FOLDER
-#exec_folder=/home/bsc32/bsc32402/local/Nemo/trunk-r10610/cfgs/ORCA2/EXP00/
+mkdir -p $RUN_FOLDER
 
-#Create the new folder
-#exp_folder=/gpfs/scratch/bsc32/bsc32402/NEMO4/run/eOrca025_ref
-exp_folder=EXP_FOLDER
-#exp_folder=/gpfs/scratch/bsc32/bsc32402/NEMO4/run/RUN_FOLDER/Orca025_4_$NEMO_PROC
+cp $blue_print_job  $RUN_FOLDER
+cd $RUN_FOLDER
 
-#Input files
-xml_folder=/gpfs/scratch/bsc32/bsc32402/NEMO4/Miguel_input_ORCA025/
-netCDF_folder=$xml_folder
-namelist_folder=$xml_folder
-
-#Restart mode
-#RESTART=True
-RESTART=False
-
-output=OUTPUT
-
-#Debug mode
-#DDT=True
-DDT=False
-
-#extrae variables
-export EXTRAE=False
-#export EXTRAE=True
-
-#activate ln_ctl flag
-#DIAGNOSTIC=True
-DIAGNOSTIC=False
-
-if [[ $RESTART == True ]]
-then
-  restart_files=/gpfs/scratch/bsc32/bsc32402/NEMO4/run/eOrca025_opt
-fi
-
-if [[ $EXTRAE == True ]]
-then 
-  #extrae_home=/apps/BSCTOOLS/extrae/3.5.2/impi_2017_4/
-  extrae_home=/apps/BSCTOOLS/extrae/3.5.4/impi_2018_1/
-  extrae_xml=/gpfs/scratch/bsc32/bsc32402/NEMO4/run/RUN_FOLDER/detailed_trace_basic.xml
-#  extrae_xml=/gpfs/scratch/bsc32/bsc32402/NEMO4/Orca2-r10610/detailed_trace_basic.xml
-  function_file=/gpfs/scratch/bsc32/bsc32402/NEMO4/run/eOrca025_scorep/extrae_functions_for_xml.txt
-fi
-
-if [[ $XIOS == True ]] 
-then
-  xios_exec_folder=/home/bsc32/bsc32402/local/XIOS/xios-2.5/bin/
-  xios_exec_name=xios_server.exe
-fi
-#SCOREP=True
-SCOREP=False
-if [[ $SCOREP == True ]]
-then
-  scorep_dir=scorep_nemo
-#  source /home/nct00/nct00004/bin/tools_x86_intel17.sh
-  module load scorep
-  export SCOREP_EXPERIMENT_DIRECTORY=$scorep_dir
-fi
-
-#file that contains version of modules to load
-impi_file=/gpfs/scratch/bsc32/bsc32402/NEMO4/run/RUN_FOLDER/impi.env
-
-#Create exp folder
-mkdir $exp_folder || exit 1
-
-#copy slurm file
-cp $0 $exp_folder
-cd $exp_folder
-
-#copy all the input files or creates the ones that doesn't exist
-cp $xml_folder/*.xml . || exit 1
-cp $namelist_folder/namelist_* . || exit 1
-cp -s $netCDF_folder/*.nc . || exit 1
-cp $netCDF_folder/*.dat . 2> /dev/null || echo "                               0  0.0000000000000000E+00  0.0000000000000000E+00" > EMPave_old.dat
-
-if [[ $RESTART == True ]]
-then
-  sed -ri 's/(.)(ln_rstart)(.*)/   \2    = .true. /' namelist_cfg   
-  sed -ri 's@(.)(cn_ocerst_indir)(.*)@   \2    =\"'$restart_files'\"@' namelist_cfg   
-fi
-if [[ $DIAGNOSTIC == True ]] 
-then
-  sed -ri 's/(.)(ln_ctl)(.*)/   \2    = .true. /' namelist_cfg
-fi
-
-
-if [[ ice == False ]] 
-then
-  sed '/def_nemo-ice/d' $xml_folder/context_nemo.xml | sed '/def_nemo-pisces/d' > context_nemo.xml
-fi
-
-if [[ outputs == False ]]
-then
-  if [[ ice == False ]] 
-  then
-    sed '/file_def/d' $xml_folder/context_nemo.xml | sed '/def_nemo-ice/d'  | sed '/def_nemo-pisces/d' > context_nemo.xml
-  else
-    sed '/file_def/d' $xml_folder/context_nemo.xml > context_nemo.xml
-  fi
-fi
-
-if ! [ -e "$impi_file"  ]
-then
-  echo $file_name ": impi file not found"
 cat << EOF > impi.env
 module purge
 module load intel/2018.3
 module load impi/2018.3
-module load netcdf/4.4.1.1
+module load netcdf/4.2
 module load hdf5/1.8.19
 module load perl
 module list
 EOF
-else
-  cp -s $impi_file .
-fi
 
-cp -s $exec_folder/$exec_name ./ || exit 1
+#XIOS=True
+XIOS=False
 
 if [[ $XIOS == True ]]
 then
-  cp -s $xios_exec_folder/$xios_exec_name ./ || exit 1
-fi
-# Finding Job id
-RN_JOBID=${SLURM_JOB_ID:-$LSB_JOBID}
-
-# Changing the number of steps (if activated)
-if [[ True == True ]];then
-   sed -ri 's/(.)(nn_itend)(.*)/   \2    =    '$time_step'   /' namelist_cfg
-   sed -ri 's/(.)(nn_itend)(.*)/   \2    =    '$time_step'   /' namelist_ref
-fi
-
-# Source the proper environment for the run
-source impi.env
-   
-# Modify the iodef.xml file to select if using or not servers.
-#if [[ False == True ]]; then
-if [[ $XIOS == True ]]; then
-    XSERVER=true
+  XIOS_PPN=4
 else
-    XSERVER=false
+  XIOS_PPN=0
 fi
 
-sed -ri 's/(.*)(using_server)(.*)(true|false)(.*)/\1\2\3'$XSERVER'\5/' iodef.xml
+#HIGHMEM=True
+HIGHMEM=False
 
-# If extrae is used, create the function file for extrae.
-if [[ $EXTRAE == True ]]
-then
-  cp $extrae_xml . || exit 1
-  cp $function_file . || exit 1
-  final_function_file=$function_file'_final'
-  if [[ $(awk '{ print NF; exit}' $function_file) -eq 1 ]]
-  then 
-    nm $exec_name | grep -i " T " | grep -w -f $function_file | awk '{print $1" # "$3}' > $final_function_file
-  elif [[ $(awk '{ print NF; exit}' $function_file) -eq 3  ]]
-  then 
-    function_file_tmp=$function_file'_tmp'
-    awk '{print $3}' $function_file >  $function_file_tmp
-    nm $exec_name | grep -i " T " | grep -w -f $function_file_tmp | awk '{print $1" # "$3}' > $final_function_file
-    rm $function_file_tmp
-  else
-    echo "Wrong format in function file \nAborting."
-    exit
-  fi  
-
-  sed 's@./extrae_functions_for_xml.txt@'$final_function_file'@' $extrae_xml > ./`basename $extrae_xml`
-  extrae_xml=`basename $extrae_xml`
-  
-  trace=trace.sh
-  cat << EOF > $trace
-  #!/bin/bash
-
-  EXTRAE_HOME=$extrae_home
-  export EXTRAE_CONFIG_FILE=$extrae_xml
-  source \$EXTRAE_HOME/etc/extrae.sh
-
-  export LD_PRELOAD=\${EXTRAE_HOME}/lib/libmpitracecf.so
-
-  export LD_LIBRARY_PATH=\${EXTRAE_HOME}/lib/:\$LD_LIBRARY_PATH
-
-  \$*
-EOF
-
-  chmod 755 $trace
-fi
-
-if [[ $xios == True ]] 
-then
-    if [[ $EXTRAE == True ]]
+PROC_PER_NODE=46
+ 
+for TOTAL_NP in  576  #`seq $((PROC_PER_NODE*51)) $((PROC_PER_NODE*4))  $((PROC_PER_NODE*100))`
+do
+  XIOS_PROC=$((XIOS_PPN*TOTAL_NP/PROC_PER_NODE))
+  NEMO_PROC=$((TOTAL_NP-XIOS_PROC))
+  # echo $XIOS_PROC $TOTAL_NP $NOP
+  job=job_$TOTAL_NP
+  cp $blue_print_job $job 
+  if [[ $HIGHMEM == True ]]
   then
-    XIOS_EXEC="-np $xios_proc ./$trace ./xios_server.exe : "
-  else
-    XIOS_EXEC="-np $xios_proc  ./xios_server.exe : "
+    sed -ri 's@HIGHMEM@SBATCH --constraint=highmem@' $job
   fi
-fi
-# Launch command
-if [[ $SCOREP == True ]]
-then
-  EXEC="time mpirun $XIOS_EXEC -np $NEMO_PROC ./$exec_name"
-fi
+  sed -ri 's@QUEUE@'$QUEUE'@' $job
+  sed -ri 's@TIME_STEP@'$TIME_STEP'@' $job
+  sed -ri 's@EXP_FOLDER@'$EXP_FOLDER'@' $job
+  sed -ri 's@EXEC_FOLDER@'$EXEC_FOLDER'@' $job
+  sed -ri 's@RUN_FOLDER@'$RUN_FOLDER'@' $job
+  sed -ri 's@TOTAL_NP@'$TOTAL_NP'@' $job
+  sed -ri 's@NEMO_PROC@'$NEMO_PROC'@' $job
+  sed -ri 's@XIOS_PROC@'$XIOS_PROC'@' $job
+  sed -ri 's@USE_XIOS@'$XIOS'@' $job
+  sed -ri 's@PROC_PER_NODE@'$PROC_PER_NODE'@' $job
+  sed -ri 's@ICE@'$ICE'@' $job
+  sed -ri 's@OUTPUT@'$OUTPUT'@' $job
 
-if [[ $EXTRAE == True ]]
-then 
-  EXEC="time mpirun $XIOS_EXEC -np $NEMO_PROC ./$trace ./$exec_name" 
-else
-  if [[ $DDT == True ]] 
-  then 
-    module load DDT/18.2 
-    EXEC="ddt --connect mpirun $XIOS_EXEC -np $NEMO_PROC ./$exec_name"
-  else
-    EXEC="time mpirun $XIOS_EXEC -np $NEMO_PROC ./$exec_name"
-  fi 
-fi
-   
-# Actual execution
-eval $EXEC
-   
+  sbatch $job 
+done
+cd .. 
