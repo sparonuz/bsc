0a9e1c33cc6bdb95cf874cb1b8808987a401e471
sparonuz <stella.paronuzzi@bsc.es>
2019-05-08 10:48:20 +0200

    BUGFIX, missing slash.

--- scaling/clean.sh
+++ scaling/clean.sh
@@ -7,12 +7,13 @@ for i in $1/*
 do
   if [[ -d $i ]] 
   then
-    rm -rf ${i}*restart*
-    rm -rf ${i}set-*
-    rm  -f  ${i}*_grid_T*
-    rm  -f  ${i}*_grid_U*
-    rm  -f  ${i}*_grid_V*
-    rm  -f  ${i}*_grid_W*
-    rm  -f  ${i}*_scalar*
+    echo $i
+    rm -rf ${i}/*restart*
+    rm -rf ${i}/set-*
+    rm  -f  ${i}/*_grid_T*
+    rm  -f  ${i}/*_grid_U*
+    rm  -f  ${i}/*_grid_V*
+    rm  -f  ${i}/*_grid_W*
+    rm  -f  ${i}/*_scalar*
   fi
 done
